c838ad04b2325de513118d358863bb0b1766b46d
Oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-07-10 13:29:17 +0100

    feat: implement Box struct with dimensions and color, add display functionality

--- submissions/week-1/day-4/Oluwagbemiga/task-2/src/main.rs
+++ submissions/week-1/day-4/Oluwagbemiga/task-2/src/main.rs
@@ -1,3 +1,36 @@
+struct Box {
+    width: i32,
+    height: i32,
+    color: Box_Color,
+}
+
+#[derive(Debug)]
+enum Box_Color {
+    Red,
+    Green,
+    Blue,
+}
+
+impl Box {
+    fn new(width: i32, height: i32, color: Box_Color) -> Box {
+        Box {
+            width,
+            height,
+            color,
+        }
+    }
+
+    fn display_dimensions(&self) {
+        println!(
+            "Box dimensions are: width of {:#?}, height of {:#?}, color of {:#?}",
+            self.width, self.height, self.color
+        );
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let my_box = Box::new(10, 20, Box_Color::Red);
+    let our_box = Box::new(100, 350, Box_Color::Green);
+    my_box.display_dimensions();
+    our_box.display_dimensions();
 }
