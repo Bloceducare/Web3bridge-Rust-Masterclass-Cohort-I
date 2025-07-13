67a73f882e5b874ee9ba15d655de91207688f941
Dean <o.michaeldean@gmail.com>
2025-07-13 18:06:27 +0100

    Added task 2 to submission for day-5 of week-1.

---
+++ submissions/week-1/day-5/Dean/books/src/main.rs
@@ -0,0 +1,66 @@
+#[derive(Debug)]
+struct Fiction {
+    name: String,
+    author: String,
+    price: f64,
+}
+
+#[derive(Debug)]
+enum BookType {
+    Fiction(Fiction),
+    SciFi { author: String, price: f64 },
+    Magazine(String, f64),
+}
+
+fn main() {
+    let books = vec![
+        BookType::Fiction(Fiction {
+            name: "Jungle Man".to_string(),
+            author: "Josh".to_string(),
+            price: 23.54,
+        }),
+        BookType::SciFi {
+            author: "Dean".to_string(),
+            price: 24.99,
+        },
+        BookType::Magazine("National Geographic Team".to_string(), 12.99),
+    ];
+
+    for (index, book) in books.iter().enumerate() {
+        print!("Book {}: ", index + 1);
+
+        match book {
+            BookType::Fiction(Fiction { name, author, price }) => {
+                // println!("Fiction - Author: {}, Price: ${:.2}", author, price);
+                println!("{:#?}", book);
+            }
+            BookType::SciFi { author, price } => {
+                // println!("Science Fiction - Price: ${:.2}", price);
+                println!("{:#?}", book);
+            }
+            BookType::Magazine(author, price) => {
+                // println!("Magazine - Author: {}, Price: ${:.2}", author, price);
+                println!("{:#?}", book);
+            }
+        }
+    }
+}
+
+
+
+
+Book 1: Fiction(
+    Fiction {
+        name: "Jungle Man",
+        author: "Josh",
+        price: 23.54,
+    },
+)
+Book 2: SciFi {
+    author: "Dean",
+    price: 24.99,
+}
+Book 3: Magazine(
+    "National Geographic Team",
+    12.99,
+)
