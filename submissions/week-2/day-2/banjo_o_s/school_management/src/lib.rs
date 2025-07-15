fa9a7d6bab53d8fa2587157611dd9b354a9c3fb9
blurbeast <leumasre@gmail.com>
2025-07-15 21:32:06 +0100

    implemented the school management and created a test module for it

--- submissions/week-2/day-2/banjo_o_s/school_management/src/lib.rs
+++ submissions/week-2/day-2/banjo_o_s/school_management/src/lib.rs
@@ -1,14 +1,86 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::collections::HashMap;
+
+
+
+mod test;
+
+
+pub struct Student {
+    pub name: String,
+    pub grade: u8,
+    pub student_status: StudentStatus,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum StudentStatus {
+    NotActive, 
+    Active
+}
+
+impl StudentStatus {
+    pub fn check_variants(self) -> Self {
+        match self {
+            Self::Active => Self::Active,
+            Self::NotActive => Self::Active
+        }
     }
 }
+
+
+impl Student {
+    pub fn new(name: String, grade: u8) -> Self {
+        Self { name: name, grade: grade, student_status: StudentStatus::Active }
+    }
+
+    pub fn update_student_status(&mut self, status: StudentStatus) -> bool {
+        self.student_status = status;
+        true
+    }
+
+    pub fn update_student_grade(&mut self, grade: u8) -> bool {
+        self.grade = grade;
+        true
+    }
+}
+
+
+
+pub struct School {
+    students: HashMap<u128, Student>,
+    student_key: u128,
+}
+
+
+impl School {
+    pub fn new() -> Self {
+        Self { students: HashMap::new(), student_key: 0 }
+    }
+
+    pub fn add_student(&mut self, name: String, grade: u8) -> bool {
+        let key = self.student_key + 1;
+        let std = Student::new(name, grade);
+        self.students.insert(key, std);
+        self.student_key += 1;
+        true
+    }
+
+    pub fn get_student(&self, key: u128) -> Option<&Student> {
+        self.students.get(&key)
+    }
+
+    pub fn remove_student(&mut self, key: u128) -> Option<Student>{
+        self.students.remove(&key)
+    }
+
+    pub fn update_student_grade(&mut self, key: u128, grade: u8) -> bool {
+        // let mut student = Self::get_student(&self, key).unwrap();
+        let student = self.students.get_mut(&key).unwrap();
+        student.update_student_grade(grade)
+    }
+    pub fn update_student_status(&mut self, key: u128, status: StudentStatus) -> bool {
+        let std = self.students.get_mut(&key).unwrap();
+        std.update_student_status(status)
+    }
+}
+
