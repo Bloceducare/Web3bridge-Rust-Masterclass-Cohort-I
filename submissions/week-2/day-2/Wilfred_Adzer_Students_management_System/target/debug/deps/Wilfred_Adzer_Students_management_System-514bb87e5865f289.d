42d99d22677f97317f00e2d306b44595ae1e0b3f
Wilfred007 <adzerwilfred007@gmail.com>
2025-07-15 12:03:12 +0100

    task for today

---
+++ submissions/week-2/day-2/Wilfred_Adzer_Students_management_System/target/debug/deps/Wilfred_Adzer_Students_management_System-514bb87e5865f289.d
@@ -0,0 +1,5 @@
+/home/wilfred/Documents/Rust_Masterclass/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/day-2/Wilfred_Adzer_Students_management_System/target/debug/deps/Wilfred_Adzer_Students_management_System-514bb87e5865f289: src/main.rs
+
+/home/wilfred/Documents/Rust_Masterclass/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/day-2/Wilfred_Adzer_Students_management_System/target/debug/deps/Wilfred_Adzer_Students_management_System-514bb87e5865f289.d: src/main.rs
+
+src/main.rs:
