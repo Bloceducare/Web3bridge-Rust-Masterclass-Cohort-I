a9c6e93410a018d1a1101158ebc6160cc2bcc7d4
Oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-07-15 12:02:04 +0100

    feat: add initial implementation of school system with Student and StudentList structs

---
+++ submissions/week-2/day-2/Oluwagbemiga/school-system/src/main.rs
@@ -0,0 +1,82 @@
+#[derive(Debug)]
+struct Student {
+    name: String,
+    active: IsActive,
+}
+
+#[derive(Debug)]
+struct StudentList {
+    students: Vec<Student>,
+}
+
+impl StudentList {
+    fn new() -> Self {
+        StudentList { students: vec![] }
+    }
+
+    fn get_student(&self, index: usize) -> Option<&Student> {
+        self.students.get(index)
+    }
+
+    fn all_students(&self) -> &Vec<Student> {
+        &self.students
+    }
+    fn get_active_students(&self) -> Vec<&Student> {
+        self.students
+            .iter()
+            .filter(|s| matches!(s.active, IsActive::Active))
+            .collect()
+    }
+
+    fn add(&mut self, name: String) {
+        self.students.push(Student::new(name));
+    }
+
+    fn remove(&mut self, index: usize) {
+        if index < self.students.len() {
+            self.students.remove(index);
+        } else {
+            println!("No student found at index {}", index);
+        }
+    }
+    fn edit(&mut self, index: usize, new_name: String) {
+        if index < self.students.len() {
+            self.students[index].name = new_name;
+        } else {
+            println!("No student found at index {}", index);
+        }
+    }
+
+    fn delete(&mut self, index: usize) {
+        if index < self.students.len() {
+            self.students.remove(index);
+        } else {
+            println!("No student found at index {}", index);
+        }
+    }
+}
+
+#[derive(Debug)]
+enum IsActive {
+    Active,
+    Inactive,
+}
+
+impl Student {
+    fn new(name: String) -> Self {
+        Student {
+            name,
+            active: IsActive::Inactive,
+        }
+    }
+}
+
+fn main() {
+    let mut student_list = StudentList::new();
+    student_list.add("Gbemiga".to_string());
+    student_list.add("Josh".to_string());
+    println!("Hello, {:?}", student_list);
+
+    student_list.edit(0, "Oluwagbemiga".to_string());
+    println!("Hello, {:#?}", student_list);
+}
