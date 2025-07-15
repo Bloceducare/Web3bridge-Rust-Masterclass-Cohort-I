f24589eb01e9b3874e1cf213201efa18841fdec6
Wilfred007 <adzerwilfred007@gmail.com>
2025-07-15 12:12:56 +0100

    latest commit

--- submissions/week-2/day-2/Wilfred_Adzer_Students_management_System/target/debug/Wilfred_Adzer_Students_management_System.d
+++
@@ -1,1 +0,0 @@
-/home/wilfred/Documents/Rust_Masterclass/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/day-2/Wilfred_Adzer_Students_management_System/target/debug/Wilfred_Adzer_Students_management_System: /home/wilfred/Documents/Rust_Masterclass/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/day-2/Wilfred_Adzer_Students_management_System/src/main.rs
