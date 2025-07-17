3f6cbb39173d55257b68df9dbd283184a0184f84
Oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-07-17 01:06:40 +0100

    Add generated files for incremental compilation and workspace metadata
    - Created new binary files: `query-cache.bin` and `work-products.bin` for incremental compilation in the Web3bridge Rust Masterclass project.
    - Added a lock file for the incremental compilation process to manage dependencies.
    - Generated `Cargo.lock` files for both the sysroot and workspace, capturing the current state of dependencies for the project.

---
+++ target/debug/deps/Web3bridge_Rust_Masterclass_Cohort_I-443d3b88a3989468.d
@@ -0,0 +1,5 @@
+C:\Users\DELL\Desktop\Web3bridge-Rust-Masterclass-Cohort-I\target\debug\deps\libWeb3bridge_Rust_Masterclass_Cohort_I-443d3b88a3989468.rmeta: src\main.rs
+
+C:\Users\DELL\Desktop\Web3bridge-Rust-Masterclass-Cohort-I\target\debug\deps\Web3bridge_Rust_Masterclass_Cohort_I-443d3b88a3989468.d: src\main.rs
+
+src\main.rs:
