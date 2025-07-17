5a344ab2c21547f1d9360366064aa351025a1ed8
Wilfred007 <adzerwilfred007@gmail.com>
2025-07-17 06:33:02 +0100

    Week 2, day-3 task:Staff access control system

---
+++ submissions/week-2/day-3/Wilfred_Adzer_Web3_Bridge_Staff_Access_Control/target/debug/build/getrandom-e262d4548f849d00/build_script_build-e262d4548f849d00.d
@@ -0,0 +1,5 @@
+/home/wilfred/Documents/Rust_Masterclass/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/day-3/Wilfred_Adzer_Web3_Bridge_Staff_Access_Control/target/debug/build/getrandom-e262d4548f849d00/build_script_build-e262d4548f849d00: /home/wilfred/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.3/build.rs
+
+/home/wilfred/Documents/Rust_Masterclass/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/day-3/Wilfred_Adzer_Web3_Bridge_Staff_Access_Control/target/debug/build/getrandom-e262d4548f849d00/build_script_build-e262d4548f849d00.d: /home/wilfred/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.3/build.rs
+
+/home/wilfred/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.3/build.rs:
---
+++ submissions/week-2/day-3/Wilfred_Adzer_Web3_Bridge_Staff_Access_Control/target/debug/deps/Wilfred_Adzer_Web3_Bridge_Staff_Access_Control-57b80a0365807369.d
@@ -0,0 +1,5 @@
+/home/wilfred/Documents/Rust_Masterclass/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/day-3/Wilfred_Adzer_Web3_Bridge_Staff_Access_Control/target/debug/deps/Wilfred_Adzer_Web3_Bridge_Staff_Access_Control-57b80a0365807369: src/main.rs
+
+/home/wilfred/Documents/Rust_Masterclass/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/day-3/Wilfred_Adzer_Web3_Bridge_Staff_Access_Control/target/debug/deps/Wilfred_Adzer_Web3_Bridge_Staff_Access_Control-57b80a0365807369.d: src/main.rs
+
+src/main.rs:
