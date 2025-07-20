c56132677799a4bd89384cda785b05c84bd47c2c
OSUOLALE <user@USERs-MacBook-Pro.local>
2025-07-21 00:44:56 +0100

    Oluwafemi modification1

--- submissions/week-2/group-projects/group-24/inventory_reorder_system/target/debug/deps/inventory_reorder_system-10c6fe62b5569d37.d
+++
@@ -1,8 +0,0 @@
-/Users/user/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/group-projects/group-24/inventory_reorder_system/target/debug/deps/inventory_reorder_system-10c6fe62b5569d37: src/main.rs src/functions.rs src/inventory.rs src/test.rs
-
-/Users/user/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/group-projects/group-24/inventory_reorder_system/target/debug/deps/inventory_reorder_system-10c6fe62b5569d37.d: src/main.rs src/functions.rs src/inventory.rs src/test.rs
-
-src/main.rs:
-src/functions.rs:
-src/inventory.rs:
-src/test.rs:
--- submissions/week-2/group-projects/group-24/inventory_reorder_system/target/debug/deps/inventory_reorder_system-f9da199f990059aa.d
+++
@@ -1,8 +0,0 @@
-/Users/user/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/group-projects/group-24/inventory_reorder_system/target/debug/deps/inventory_reorder_system-f9da199f990059aa: src/main.rs src/functions.rs src/inventory.rs src/test.rs
-
-/Users/user/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/group-projects/group-24/inventory_reorder_system/target/debug/deps/inventory_reorder_system-f9da199f990059aa.d: src/main.rs src/functions.rs src/inventory.rs src/test.rs
-
-src/main.rs:
-src/functions.rs:
-src/inventory.rs:
-src/test.rs:
