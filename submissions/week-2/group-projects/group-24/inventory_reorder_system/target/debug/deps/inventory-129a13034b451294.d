87198e9b518061ce781386980c5c0b6d2b22e307
OSUOLALE <user@USERs-MacBook-Pro.local>
2025-07-21 00:45:46 +0100

    Oluwafemi modification1

--- submissions/week-2/group-projects/group-24/inventory_reorder_system/target/debug/deps/inventory-129a13034b451294.d
+++
@@ -1,7 +0,0 @@
-/Users/user/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/group-projects/group-24/inventory_reorder_system/target/debug/deps/libinventory-129a13034b451294.rmeta: /Users/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/inventory-0.3.20/src/lib.rs
-
-/Users/user/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/group-projects/group-24/inventory_reorder_system/target/debug/deps/libinventory-129a13034b451294.rlib: /Users/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/inventory-0.3.20/src/lib.rs
-
-/Users/user/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/group-projects/group-24/inventory_reorder_system/target/debug/deps/inventory-129a13034b451294.d: /Users/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/inventory-0.3.20/src/lib.rs
-
-/Users/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/inventory-0.3.20/src/lib.rs:
--- submissions/week-2/group-projects/group-24/inventory_reorder_system/target/debug/deps/inventory_reorder_system-7179daf11e3288a5.d
+++
@@ -1,8 +0,0 @@
-/Users/user/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/group-projects/group-24/inventory_reorder_system/target/debug/deps/inventory_reorder_system-7179daf11e3288a5: src/main.rs src/functions.rs src/inventory.rs src/test.rs
-
-/Users/user/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/group-projects/group-24/inventory_reorder_system/target/debug/deps/inventory_reorder_system-7179daf11e3288a5.d: src/main.rs src/functions.rs src/inventory.rs src/test.rs
-
-src/main.rs:
-src/functions.rs:
-src/inventory.rs:
-src/test.rs:
