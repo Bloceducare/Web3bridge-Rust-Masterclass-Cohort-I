bca157dcda4580395c4e6ee2234dffbeca0e2e23
blurbeast <leumasre@gmail.com>
2025-07-21 12:33:55 +0100

    done

--- submissions/week-2/group-projects/group_4/events/src/main.rs
+++ submissions/week-2/group-projects/group_4/events/src/main.rs
@@ -1,7 +1,12 @@
 use crate::event::io::helpers::helpers::start_app;
+use std::{thread, time::Duration};
 
 pub mod event;
 
 fn main() {
+    for i in (1..=5).rev() {
+        println!("Starting in {}...", i);
+        thread::sleep(Duration::from_secs(1));
+    }
     start_app();
 }
