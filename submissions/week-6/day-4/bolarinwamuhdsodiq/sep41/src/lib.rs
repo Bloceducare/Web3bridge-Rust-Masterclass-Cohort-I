ac70454c05763fe5aca8c58bb7b30c4a54bc3ba7
Amity808 <bolarinwamuhdsodiq0@gmail.com>
2025-08-18 08:37:18 +0100

    sep41 implementation

---
+++ submissions/week-6/day-4/bolarinwamuhdsodiq/sep41/src/lib.rs
@@ -0,0 +1,35 @@
+// writing sep41 contract 
+
+#![no_std]
+
+// use core::ops::Add;
+
+mod admin;
+mod allowance;
+mod balance;
+mod metadata;
+mod storage_types;
+mod contract;
+
+// pub use crate::contract;
+
+
+// use soroban_sdk::{contract, contractclient, contractimpl, Address, Env, String};
+
+// #[contractclient( name = "TokenClient")]
+// pub trait  Token {
+//         fn allowance(env: Env, from: Address, to: Address) -> i128;
+//         fn approve(env: Env, from: Address, spender: Address, amount: i128, live_until_ledger: u32);
+//         fn balance(env: Env, id: Address) -> i128;
+//         fn transfer(env: Env, from: Address, to: Address, amount: i128);
+//         fn transfer_from(env: Env, spender: Address, from: Address, to: Address, amount: i128);
+//         fn burn(env: Env, from: Address, amount: i128);
+//         fn burn_from(env: Env, spender: Address, from: Address, amount: i128);
+
+//         fn decimals(env: Env) -> u32;
+//         fn name(env: Env) -> String;
+//         fn symbol(env: Env) -> String;
+
+//         fn mint(env: Env, to: Address, amount: i128, live_until_ledger: u32);
+// }
+
